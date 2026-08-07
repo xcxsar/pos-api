21f018da82839a3164a33e23ad7422998da2cfc9
xcxsar <l2225010052@guasave.tecnm.mx>
2026-08-06 21:45:51 -0700

    feat: Enhance database connection handling

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/xcxsar/pos-api/internal/store/sqlc"
 )
+
+type apiConfig struct {
+	queries   *sqlc.Queries
+	db        *sql.DB
+	jwtSecret string
+}
 
 func main() {
 	if err := godotenv.Load("../../.env"); err != nil {
@@ -14,6 +23,28 @@ func main() {
 	}
 
 	port := os.Getenv("PORT")
+	dbUrl := os.Getenv("DB_URL")
+
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal("failed to open DB pool:", err)
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("failed to connect to the database:", err)
+	}
+
+	log.Println("connection to database established")
+
+	dbQueries := sqlc.New(db)
+
+	var apiCfg apiConfig
+
+	apiCfg.queries = dbQueries
+	apiCfg.db = db
 
 	mux := http.NewServeMux()
 
@@ -22,6 +53,6 @@ func main() {
 		Handler: mux,
 	}
 
-	log.Println("Server started, listening at localhost:" + port)
+	log.Println("server started, listening at localhost:" + port)
 	log.Fatal(server.ListenAndServe())
 }
