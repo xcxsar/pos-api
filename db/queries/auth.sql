caa6df2202b22dbd6d963066a158286d9db053ec
xcxsar <l2225010052@guasave.tecnm.mx>
2026-08-06 19:22:54 -0700

    feat: Implement user management queries and models

---
+++ db/queries/auth.sql
@@ -0,0 +1,18 @@
+-- name: CreateUser :one
+INSERT INTO users (id, created_at, updated_at, email, hashed_password)
+VALUES (gen_random_uuid(), now(), now(), $1, $2)
+RETURNING id, created_at, updated_at, email;
+
+-- name: GetUserByEmail :one
+SELECT * FROM users WHERE email = $1;
+
+-- name: UpdateUserEmail :one
+UPDATE users SET email = $1, updated_at = now() WHERE id = $2
+RETURNING id, created_at, updated_at, email;
+
+-- name: UpdateUserPassword :one
+UPDATE users SET hashed_password = $1, updated_at = now() WHERE id = $2
+RETURNING id, created_at, updated_at, email;
+
+-- name: DeleteUser :exec
+DELETE FROM users WHERE id = $1;
